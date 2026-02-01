1d4c56897cef4debcf6d5ce72a2ba3a441d2a78d
Daniele Mazzola <dm.daniele94@gmail.com>
2026-02-01 13:51:38 +0100

    Update sift-create.sh

--- files/sift-create.sh
+++ files/sift-create.sh
@@ -7,23 +7,24 @@ echo "--- Creating Distrobox container: $CONTAINER_NAME ---"
 # We use Ubuntu 22.04 as it is the most stable base for SIFT SaltStack
 distrobox create --name $CONTAINER_NAME --image ubuntu:22.04 --yes
 
-echo "--- Starting installation inside the container ---"
-# Execute commands inside the newly created container
-distrobox enter $CONTAINER_NAME -- bash -c "
-    sudo apt-get update && sudo apt-get install -y wget curl gnupg2;
-
-    # Ci spostiamo in /tmp per non sporcare la home
+echo "--- Avvio installazione interna ---"
+# Usiamo -e per fermarci al primo errore e vedere cosa succede
+distrobox enter $CONTAINER_NAME -- bash -e -c "
+    echo '1. Aggiornamento repository...'
+    sudo apt-get update
+    
+    echo '2. Installazione dipendenze base...'
+    sudo apt-get install -y wget curl gnupg2 ca-certificates
+    
+    echo '3. Download CAST v1.0.4...'
     cd /tmp
+    wget -c https://github.com/teamdfir/cast/releases/download/v1.0.4/cast-v1.0.4-linux-amd64.deb
     
-    echo '--- Downloading CAST v1.0.4 ---';
-    wget https://github.com/ekristen/cast/releases/tag/v1.0.4/cast-v1.0.4-linux-amd64.deb;
+    echo '4. Installazione CAST...'
+    sudo dpkg -i cast-v1.0.4-linux-amd64.deb || sudo apt-get install -f -y
     
-    echo '--- Installing CAST ---';
-    sudo dpkg -i cast-v1.0.4-linux-amd64.deb || sudo apt-get install -f -y;
-    
-    echo '--- Starting SIFT Installation (Server Mode) ---';
-    # Server mode is recommended for USB drives to save space and improve performance
-    sudo cast install teamdfir/sift-saltstack --mode=server;
+    echo '5. Installazione SIFT (Server Mode)...'
+    sudo cast install teamdfir/sift-saltstack --mode=server
 "
 
 echo "--- Setup Finished! ---"
